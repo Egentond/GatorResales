a72ee0731defa833738342e4940369f023c2fe0a
egentond <egentond@tcd.ie>
2024-10-29 16:20:16 -0400

    Fixed imports in the main.jsx

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -8,6 +8,8 @@ import Home from './components/Home.jsx';
 import PageNotFound from './components/PageNotFound.jsx';
 import ListTicket from './components/ListTicket.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
+import Buy from './components/Buy.jsx';
+import Sell from './components/Sell.jsx';
 
 
 const router = createBrowserRouter([
