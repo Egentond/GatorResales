3b4c3ac8cf9f4ffc385bd1497112eafd718059d0
egentond <egentond@tcd.ie>
2024-12-01 17:13:55 -0500

    added filtering tickets route

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { listTicket, getTickets } = require('../controllers/ticketController');
+const { listTicket, getTickets, filterTicketsBySport } = require('../controllers/ticketController');
 const { protect } = require('../middleware/auth')
 
 router.post('/list-ticket', listTicket);
-router.get('./get-ticket', getTickets);
+router.get('/get-tickets', getTickets);
+router.get('/get-tickets-by-sport', filterTicketsBySport);
 
 module.exports = router;
