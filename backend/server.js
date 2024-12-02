bc8302984eee63474b55c797a456bb929a34b1e9
adamachd <adamachd@tcd.ie>
2024-12-01 22:10:47 -0500

    stripe checkout complete

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,6 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const userRoutes = require('./routes/userRoutes');
-
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
@@ -17,9 +15,12 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+// User and ticket routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
-// app.use('/stripe', require('./routes/stripeRoutes'));
+
+// Stripe routes
+app.use('/api/stripe', require('./routes/stripeRoutes'));
 
 const uri = process.env.MONGODB_URI;
 
